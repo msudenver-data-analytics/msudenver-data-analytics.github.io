d7ed0f7a7a58ec94e6116d5ae2888dfc20d544b8
msudenver-data-analytics <120525339+msudenver-data-analytics@users.noreply.github.com>
2023-02-24 10:37:36 -0700

    updates bubble size buckets

--- projects/zig_zaggy_lives/js/moving_bubbles.js
+++ projects/zig_zaggy_lives/js/moving_bubbles.js
@@ -362,6 +362,9 @@ async function loadStudentDataAndInitalizeNodes(fileName) {
   if (studentData.length > 2000) {
     simulationRate = 8000;
     bubbleRadius = 4;
+  } else if (studentData.length > 1500) {
+    simulationRate = 6000;
+    bubbleRadius = 5;
   } else if (studentData.length > 400) {
     simulationRate = 5000;
     bubbleRadius = 6;
